0d34007721a3f08c05e0c5344d4ac3a52271c69d
Danilo Freire <danilofreire@gmail.com>
2024-08-12 01:53:36 +0100

    remove docs

--- docs/docs/tutorials/testing-anaconda.py
+++
@@ -1,8 +0,0 @@
-# This script is used to test if the Anaconda environment is working properly
-
-# Import the sys module. It comes with Python, so no need to install it
-import sys
-
-# Print the version of Python and the path to the Python executable
-print(sys.version)
-print(sys.executable)
